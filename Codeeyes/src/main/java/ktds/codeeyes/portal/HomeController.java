ad6ebe35e5a184f97ca3993c421a3fe129696015
김영은 <euni801@naver.com>
2022-03-23 14:46:09 +0900

    test

--- Codeeyes/src/main/java/ktds/codeeyes/portal/HomeController.java
+++ Codeeyes/src/main/java/ktds/codeeyes/portal/HomeController.java
@@ -2,10 +2,12 @@ package ktds.codeeyes.portal;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,21 +19,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	@Autowired
+	HomeDao homeDao;
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home(Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		List<HomeDto> list = homeDao.select();
+
+		for(int i=0; i<list.size(); i++){
+
+			logger.info(list.get(i).getNo());
+
+			logger.info(list.get(i).getName());
+
+			model.addAttribute("name", list.get(0).getName());
+
+		}
 		
 		return "home";
 	}
---
+++ Codeeyes/src/main/java/ktds/codeeyes/portal/HomeDto.java
@@ -0,0 +1,20 @@
+package ktds.codeeyes.portal;
+
+public class HomeDto {
+
+	private String no;
+	private String name;
+
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
